136d0c633e6f4ab5cb0490ee1b19f067bdc109c4
Joseph Hayfron Acquah <joeacq81@gmail.com>
2024-01-04 19:16:57 +0000

    Initial commit

---
+++ src/views/EventList.vue
@@ -0,0 +1,27 @@
+<script setup>
+import EventCard from '@/components/EventCard.vue'
+import EventService from '../services/EventServices'
+import { ref, onMounted } from 'vue'
+
+const events = ref(null)
+
+onMounted(() => {
+  EventService.getEvents()
+    .then((response) => (events.value = response.data))
+    .catch((err) => console.log('Error Occurred'))
+})
+</script>
+
+<template>
+  <main id="wrapper">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </main>
+</template>
+
+<style scoped>
+#wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
